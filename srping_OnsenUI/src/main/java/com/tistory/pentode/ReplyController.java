c16b41b5db2983e2877ec893b852a06f75824280
kyc <itsogkyc@gmail.com>
2018-03-06 17:55:49 +0900

    spring onsen UI 사용하여 꾸미기

---
+++ srping_OnsenUI/src/main/java/com/tistory/pentode/ReplyController.java
@@ -0,0 +1,66 @@
+package com.tistory.pentode;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tistory.pentode.service.BoardService;
+import com.tistory.pentode.vo.CommentVO;
+
+@RestController
+@RequestMapping("/comment")
+public class ReplyController {
+	
+	@Resource(name = "boardService")
+	private BoardService boardService;
+	
+//    @RequestMapping(value = "/list", method=RequestMethod.GET) //댓글 리스트
+//    public ResponseEntity<List<CommentVO>> mCommentServiceList() throws Exception{
+//    	ResponseEntity<List<CommentVO>> entity =null;
+//		entity = new ResponseEntity<>(boardService.commentList(),HttpStatus.OK);
+//    	return entity;
+//    }
+    
+	
+	@RequestMapping("/list")
+    public List<CommentVO> listJson(@RequestParam("bno") int bno) throws Exception {
+        System.out.println(bno);
+		List<CommentVO> list = boardService.commentList(bno);
+        return list; 
+    }
+	
+	
+	//댓글 등록
+	@RequestMapping("/insert")
+	public int commentInsert(@RequestParam("bno") int bno, @RequestParam("content") String content) throws Exception {
+		CommentVO comment = new CommentVO();
+		comment.setBno(bno);
+		comment.setContent(content);
+		return boardService.commentInsert(comment);
+	}
+	
+	
+	//댓글 삭제
+    @RequestMapping("/delete/{cno}")   
+    private int commentDelete(@PathVariable int cno) throws Exception{
+        System.out.println("나호출했음");
+        
+    	return boardService.commentDelete(cno);
+    }
+    
+    //댓글 수정
+    @RequestMapping("/update")   
+    private int commentUpdate(@RequestParam int cno, @RequestParam String content) throws Exception{
+        CommentVO comment = new CommentVO();
+        comment.setCno(cno);
+        comment.setContent(content);
+        return boardService.commentUpdate(comment);
+    }
+    
+    
+}
---
+++ srping_OnsenUI/src/main/java/com/tistory/pentode/vo/BoardVO.java
@@ -0,0 +1,69 @@
+package com.tistory.pentode.vo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BoardVO {
+	private Integer num;
+	private String title;
+	private String content;
+	private Integer readCount;
+	private String writeDate;
+	private Integer nowPage;
+	private Integer reply;
+	
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Integer getReadCount() {
+		return readCount;
+	}
+	public void setReadCount(Integer readCount) {
+		this.readCount = readCount;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(Date writeDate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		String today = sdf.format(writeDate); 
+		System.out.println(today);
+		this.writeDate = today;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVO [num=" + num + ", " + ", title=" + title + ", content=" + content + ", readCount="
+				+ readCount + ", writeDate=" + writeDate + "]";
+	}
+	
+	public Integer getNowPage() {
+		return nowPage;
+	}
+	
+	public void setNowPage(Integer nowPage) {
+		this.nowPage = nowPage;
+	}
+	public Integer getReply() {
+		return reply;
+	}
+	public void setReply(Integer reply) {
+		this.reply = reply;
+	}
+
+}
